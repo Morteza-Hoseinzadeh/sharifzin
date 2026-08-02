ffd32fac41f9d26a03051bc06878501953ff39a4
Morteza-hosseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2026-08-02 17:09:42 +0330

    Develop product card page

--- src/components/custom/Product-Card/ProductCard.jsx
+++ src/components/custom/Product-Card/ProductCard.jsx
@@ -1,54 +1,72 @@
-import { alpha, Box, Button, Divider, IconButton, Typography, useTheme } from '@mui/material';
+import { alpha, Box, IconButton, Typography, useTheme } from '@mui/material';
 import { Share } from 'iconsax-reactjs';
 import React from 'react';
 
 export default function ProductCard({ item }) {
   const theme = useTheme();
-  const overlay = '/assets/svg-overlays/hero-section-center.svg';
+  const overlay = '/assets/svg-overlays/product-card.svg';
 
   return (
-    <Box display={'flex'} flexDirection={'column'} justifyContent={'space-between'} gap={2} alignItems={'center'} sx={{ width: '100%', height: 'fit-content', backgroundColor: '#fff', borderRadius: '32px' }}>
-      <Box sx={{ backgroundColor: '#D7E0E6', p: 2, mt: 2, borderRadius: '16px' }}>
-        <Box sx={{ p: 2, backgroundColor: '#fff', borderRadius: '16px' }}>
-          <img src={item?.thumbnail} alt={item?.title} width={250} />
+    <Box display="flex" flexDirection="column" gap={2} sx={{ width: '100%', backgroundColor: '#fff', borderRadius: '32px', py: 1, px: 1.5, boxShadow: '0 0 30px #00000015' }}>
+      <Box sx={{ backgroundColor: '#D7E0E6', p: 2, borderRadius: '24px' }}>
+        <Box sx={{ backgroundColor: '#fff', borderRadius: '16px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <img src={item?.thumbnail} alt={item?.title} width="100%" style={{ maxWidth: 250 }} />
         </Box>
-        <Box display={'flex'} alignItems={'center'} justifyContent={'space-between'} mt={2} gap={1}>
-          <Box width={'100%'} display={'flex'} alignItems={'center'} justifyContent={'space-between'} gap={2} p={'8px 12px'} sx={{ backgroundColor: '#fff', borderRadius: '12px' }}>
-            <Box textAlign={'right'}>
-              <Typography variant="body2">دسته‌بندی</Typography>
-              <Typography variant="body2">{item?.category}</Typography>
+
+        <Box width={'100%'} position={'relative'}>
+          <Box width={'75%'} display="flex" alignItems="center" justifyContent="space-between" mt={2} gap={1}>
+            <Box textAlign="center" sx={{ backgroundColor: '#fff', borderRadius: '12px', px: 2, py: 1, flex: 1 }}>
+              <Typography variant="caption" color="text.disabled" display="block">
+                دسته‌بندی
+              </Typography>
+              <Typography variant="body2" fontWeight={500} noWrap>
+                {item?.category}
+              </Typography>
             </Box>
-            <Box sx={{ width: '1px', height: 25, display: 'inline-block', backgroundColor: '#eee' }} />
-            <Box textAlign={'right'}>
-              <Typography variant="body2">مدل</Typography>
-              <Typography variant="body2">{item?.model}</Typography>
+
+            <Box textAlign="center" sx={{ backgroundColor: '#fff', borderRadius: '12px', px: 2, py: 1, flex: 1 }}>
+              <Typography variant="caption" color="text.disabled" display="block">
+                مدل
+              </Typography>
+              <Typography variant="body2" fontWeight={500} noWrap>
+                {item?.model}
+              </Typography>
             </Box>
           </Box>
           <Box>
-            <Button sx={{ backgroundColor: theme.palette.primary.main, borderRadius: '16px', width: 55, height: 55 }}>
-              <Share size={26} variant="Bulk" color={theme.palette.primary.contrastText} />
-            </Button>
+            <Box position={'absolute'} bottom={-25} left={-17} zIndex={1}>
+              <img src={overlay} alt={`${item?.title} اشتراک گذاری محصول`} style={{ maxWidth: 85 }} />
+            </Box>
+            <Box position={'absolute'} bottom={-5} left={-15} zIndex={2}>
+              <IconButton sx={{ backgroundColor: theme.palette.primary.main, borderRadius: '16px', width: 55, height: 55, display: 'flex', alignItems: 'center', justifyContent: 'center', flexShrink: 0, transition: 'all ease 0.2s', '&:hover': { backgroundColor: theme.palette.primary.dark } }}>
+                <Share size={26} variant="Bulk" color="#fff" />
+              </IconButton>
+            </Box>
           </Box>
         </Box>
       </Box>
-      <Box width={'100%'} textAlign={'right'} px={2} pb={2} display={'flex'} flexDirection={'column'}>
-        <Typography variant="h6" component={'h1'}>
+
+      <Box width="100%" textAlign="right" p={1} display="flex" flexDirection="column">
+        <Typography variant="h6" component="h1" fontWeight={700}>
           {item?.title}
         </Typography>
-        <Typography variant="body2" component={'span'} color="text.disabled">
+        <Typography variant="body2" component="span" color="text.disabled">
           {item?.description}
         </Typography>
-        <Box my={0.5}>
+
+        <Box my={1.5}>
           <Box sx={{ width: '100%', height: 1.5, backgroundColor: alpha(theme.palette.text.disabled, 0.1), display: 'inline-block' }} />
         </Box>
-        <Box width={'100%'} display={'flex'} alignItems={'center'} justifyContent={'space-between'}>
+
+        <Box width="100%" display="flex" alignItems="center" justifyContent="space-between">
           <Typography variant="body1" color="text.disabled">
             قیمت:
           </Typography>
-          <Box>
-            <Typography variant="h5" fontWeight={600}>
-              {item?.finalPrice.toLocaleString('fa-IR')}
+          <Box display="flex" alignItems="center" justifyContent="flex-end" gap={1}>
+            <Typography variant="h5" fontWeight={700}>
+              {item?.finalPrice?.toLocaleString('fa-IR')}
             </Typography>
+            <img src="/assets/svg-overlays/toman-overlay.svg" width={22} height={22} alt="تومان" />
           </Box>
         </Box>
       </Box>
